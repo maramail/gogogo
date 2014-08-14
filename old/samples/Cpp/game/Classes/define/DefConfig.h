9733481a249a28e43b0cc0f74de6fa83edbae060
maramail <398722977@qq.com>
2014-08-14 23:33:05 +0800

    222333333

--- old/samples/Cpp/game/Classes/define/DefConfig.h
+++
@@ -1,7 +0,0 @@
-#ifndef _DEFINE_CONFIG_
-#define _DEFINE_CONFIG_
-
-#define CONFIG_PATH "config/";
-#define CONFIG_GRAPHICAL "graphical.json";
-
-#endif // !_DEFINE_CONFIG_
--- old/samples/Cpp/game/Classes/util/UtilView.h
+++
@@ -1,8 +0,0 @@
-#ifndef _UTIL_VIEW_
-#define _UTIL_VIEW_
-class UtilView
-{
-public: 
-	static float getViewDistanceX();
-};
-#endif // !_UTIL_VIEW_
